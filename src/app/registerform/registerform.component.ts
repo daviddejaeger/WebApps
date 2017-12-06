2b6a6a759bf3e12f1dfff30b93b2a059df38fdb5
BEICTASS <BEICTASS@FYTOLAB.BE>
2017-12-06 16:13:16 +0100

    register and welcome components afgewerkt

--- src/app/registerform/registerform.component.ts
+++ src/app/registerform/registerform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from '../user';
 
 @Component({
   selector: 'app-registerform',
@@ -6,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registerform.component.css']
 })
 export class RegisterformComponent implements OnInit {
-
+  user: any = {
+    voornaam: "",
+    achternaam: "",
+    email: "",
+    wachtwoord: "",
+    wachtwoord2: "",
+    isAdmin: false
+  };
   constructor() { }
 
   ngOnInit() {
+    
   } 
   registerUser(){
-    
+    console.log("register User called, voornaam: " + this.user.voornaam);
   }
 
 }
